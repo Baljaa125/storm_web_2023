dfc0b5827c6a366fa3769e444179afda3cae7e7c
Storm-8 <storm-8@Admin-8s-iMac.local>
2023-11-16 12:12:40 +0800

    react

---
+++ lesson_day_28_react/src/App.jsx
@@ -0,0 +1,20 @@
+
+import Footer from "./components/Footer"
+import Main from "./components/MainContent"
+import Navbar from "./components/Navbar"
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Main/>
+      <Footer />
+
+    </div>
+
+  )
+}
+
+export default App
---
+++ lesson_day_28_react/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <header><nav>
+            <ul>
+                <li><a href="#Home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+        </header>
+    )
+}
+export default Navbar;
